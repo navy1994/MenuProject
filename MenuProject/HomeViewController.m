bc0bb9d3318581d5c0e8b73bda647ba0d04728b7
navy1994 <lihaijun1994@gmail.com>
2015-10-14 15:36:40 +0800

    添加了轮播广告

--- MenuProject/HomeViewController.m
+++ MenuProject/HomeViewController.m
@@ -12,8 +12,17 @@
 #import <DYMRollingBanner/DYMRollingBannerVC.h>
 #import "UIImage+UIColor.h"
 
+const CGFloat LSWHeaderViewHeight = 180;
 
-@interface HomeViewController ()
+@interface HomeViewController ()<UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate>{
+    DYMRollingBannerVC      *_rollingBannerVC;
+    MASConstraint *_headerViewLeftConstraint;//左边约束
+    MASConstraint *_headerViewWidthConstraint;//宽的约束
+    MASConstraint *_headerViewTopConstraint;//上边的约束
+    MASConstraint *_headerViewHeightConstraint;//高的约束
+}
+
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
@@ -22,8 +31,99 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
-    // Do any additional setup after loading the view.
+    
+    [self setupUI];
 }
+
+- (void)setupUI{
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage imageWithColor:[[UIColor clearColor] colorWithAlphaComponent:0]] forBarMetrics:UIBarMetricsDefault];
+    [self.navigationController.navigationBar setShadowImage:[[UIImage alloc] init]];
+    self.navigationController.navigationBarHidden = YES;
+    
+    _tableView = [[UITableView alloc]initWithFrame:[UIScreen mainScreen].bounds style:UITableViewStyleGrouped];
+    [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    _tableView.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    _tableView.separatorStyle = NO;
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    _tableView.contentInset= UIEdgeInsetsMake(LSWHeaderViewHeight, 0, 0, 0);
+    [self.view addSubview:_tableView];
+    
+    _rollingBannerVC = [DYMRollingBannerVC new];
+    [self addChildViewController:_rollingBannerVC];
+    [_tableView addSubview:_rollingBannerVC.view];
+    
+    [_rollingBannerVC.view mas_makeConstraints:^(MASConstraintMaker *make) {
+        _headerViewLeftConstraint = make.left.equalTo(@0);
+        _headerViewWidthConstraint = make.width.equalTo(@(_tableView.frame.size.width));
+        _headerViewTopConstraint = make.top.equalTo(@(-LSWHeaderViewHeight));
+        _headerViewHeightConstraint = make.height.equalTo(@(LSWHeaderViewHeight));
+    }];
+    
+    [_rollingBannerVC didMoveToParentViewController:self];
+    
+    _rollingBannerVC.rollingInterval = 5;
+    _rollingBannerVC.rollingImages = @[[UIImage imageNamed:@"advert1"]
+                                       , [UIImage imageNamed:@"advert2"]
+                                       , [UIImage imageNamed:@"advert3"]
+                                       , [UIImage imageNamed:@"advert2"]    // Local Image
+                                       , [UIImage imageNamed:@"advert3"]    // Locak Image
+                                       ];
+    
+    [_rollingBannerVC addBannerTapHandler:^(NSInteger whichIndex) {
+        NSLog(@"banner tapped, index = %@", @(whichIndex));
+    }];
+    
+    [_rollingBannerVC startRolling];
+    
+}
+
+#pragma mark - tableViewDelegate&DataSource
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 150;
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 3;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    if (cell==nil) {
+        cell=[[UITableViewCell alloc]init];
+    }
+    
+    cell.contentView.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    return cell;
+}
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGFloat yOffset  = scrollView.contentOffset.y;
+    CGFloat xOffset = (yOffset + LSWHeaderViewHeight)/2;
+    if (yOffset < -LSWHeaderViewHeight) {
+        _headerViewTopConstraint.offset = yOffset;
+        _headerViewLeftConstraint.offset = xOffset;
+        _headerViewWidthConstraint.offset = _tableView.frame.size.width + fabs(xOffset) * 2;
+        _headerViewHeightConstraint.offset = -yOffset;
+    }
+    //    CGFloat alpha = (yOffset + LSWHeaderViewHeight)/LSWHeaderViewHeight;
+    //    [self.navigationController.navigationBar setBackgroundImage:[UIImage imageWithColor:[ColorRGB(94.0, 158.0, 236.0) colorWithAlphaComponent:alpha]] forBarMetrics:UIBarMetricsDefault];
+    //    alpha = fabs(alpha);
+    //    alpha = fabs(1 - alpha);
+    //    alpha = alpha < 0.5 ? 0.5 : alpha;
+    //    _rollingBannerVC.view.alpha = alpha;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
